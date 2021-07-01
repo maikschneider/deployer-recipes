c4c95c4abc30d42d4e75ae21cef3cbf24c89d07c
Maik Schneider <m.schneider@blueways.de>
2021-07-01 13:10:55 +0200

    fix: setting defaults

--- recipe/defaults.php
+++ recipe/defaults.php
@@ -2,19 +2,17 @@
 
 namespace Deployer;
 
-set('web_path', get('web_path', 'public/'));
+set('web_path', 'public/');
 
-set('shared_files', get('shared_files',
-    [
-        '.env',
-        'public/.htaccess',
-        'public/typo3conf/LocalConfiguration.php',
-        'public/typo3conf/AdditionalConfiguration.php',
-        'public/typo3conf/PackageStates.php'
-    ]));
+set('shared_files', [
+    '.env',
+    'public/.htaccess',
+    'public/typo3conf/LocalConfiguration.php',
+    'public/typo3conf/AdditionalConfiguration.php',
+    'public/typo3conf/PackageStates.php'
+]);
 
-// set default backup paths for media
-set('file_backup_packages', get('file_backup_packages', [
+set('file_backup_packages', [
     'media' => [
         [
             '-path "./public/fileadmin/*"',
@@ -25,4 +23,4 @@ set('file_backup_packages', get('file_backup_packages', [
             '-path "./public/uploads/*"',
         ]
     ]
-]));
+]);
