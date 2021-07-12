a14ac2eee38a7cb338755528352e8613abf1887c
Maik Schneider <m.schneider@blueways.de>
2021-07-12 14:41:28 +0200

    feat: use official deployer recipes, use fork of deployer-extended-typo3

--- autoload.php
+++ autoload.php
@@ -6,6 +6,9 @@ $vendorDir = substr(dirname(dirname(__DIR__)), -6, 6) === 'vendor' ? __DIR__ . '
 require_once($vendorDir . 'sourcebroker/deployer-loader/autoload.php');
 new \SourceBroker\DeployerExtendedTypo3\Loader();
 
+// require default deployer recipies
+require_once($vendorDir . 'deployer/recipes/recipe/slack.php');
+
 // install own recipes
 require_once(__DIR__ . '/recipe/defaults.php');
 require_once(__DIR__ . '/recipe/db_backup_rsync.php');
