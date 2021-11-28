3db017ed74a393aaccb7f438bb3a164d8861e789
Maik Schneider <m.schneider@blueways.de>
2021-11-28 20:14:43 +0100

    feat: automatically add own ssh key to host, checks for git, composer, correct versions

--- recipe/deploy_authorize_bitbucket.php
+++ recipe/deploy_authorize_bitbucket.php
@@ -2,7 +2,7 @@
 
 namespace Deployer;
 
-task('deploy:authorize:bitbucket', function(){
+task('deploy:authorize:bitbucket', function () {
 
     // check stage
     if (empty(get('argument_stage'))) {
@@ -18,8 +18,9 @@ task('deploy:authorize:bitbucket', function(){
 
     // check user authentication
     $localKey = runlocally('cat ~/.ssh/id_rsa.pub');
-    if (!test('[[ $(grep "'. $localKey.'" ~/.ssh/authorized_keys) ]]')) {
-        $doAuthorize = askConfirmation('Your ssh keys are not in ~/.ssh/authorized_keys. Do you want to authenticate yourself?', true);
+    if (!test('[[ $(grep "' . $localKey . '" ~/.ssh/authorized_keys) ]]')) {
+        $doAuthorize = askConfirmation('Your ssh keys are not in ~/.ssh/authorized_keys. Do you want to authenticate yourself?',
+            true);
         if ($doAuthorize) {
             writeln('Adding key..');
             run('echo "' . $localKey . '" >> ~/.ssh/authorized_keys');
@@ -33,22 +34,46 @@ task('deploy:authorize:bitbucket', function(){
     }
 
     // check bitbucket rsa access
-    if(!test('[[ $(ssh-keygen -F bitbucket.org) ]]')) {
+    if (!test('[[ $(ssh-keygen -F bitbucket.org) ]]')) {
         writeln('bitbucket.org is not not a known_host, generating key locally and adding it to {{hostname}}..');
         $key = runLocally('ssh-keyscan -t rsa -H bitbucket.org');
         run('echo "' . $key . '" >> ~/.ssh/known_hosts');
     }
 
-    // check git access
+    // check if git is installed
+    $notInstalledProgramms = [];
+    if (!test('[[ $(which git) ]]')) {
+        $notInstalledProgramms[] = 'git';
+    }
+
+    // check if composer is installed
+    if (!test('[[ $(which composer) ]]')) {
+        $notInstalledProgramms[] = 'composer (version 2.x)';
+    }
+
+    if (test('[[ $(which composer) ]]')) {
+        $composerVersion = run('composer --version 2>/dev/null | egrep -o "([0-9]{1,}\.)+[0-9]{1,}"');
+        $composerVersion = explode('.', $composerVersion);
+        if ((int)$composerVersion[0] !== 2) {
+            $notInstalledProgramms[] = 'composer (version 2.x)';
+        }
+    }
+
+    if (!empty($notInstalledProgramms)) {
+        writeln('The following programs are not installed: ' . implode($notInstalledProgramms) . '.');
+        writeln('Please install the programs and re-run this command');
+        return;
+    }
+
+    // check repository access
     writeln('{{repository}}');
 
+    run('git ls-remote -h {{repository}}');
 
     //$key = runLocally('ssh-keygen -F bitbucket.org || ssh-keyscan -t rsa -H bitbucket.org');
 
     //test()
 
     //run('echo "' . $key . '" >> ~/.ssh/known_host');
-
-
 
 });
