d1b2f0f2bb4909ba24126fa1d151168d802a8b2e
Maik Schneider <m.schneider@blueways.de>
2021-11-29 15:45:46 +0100

    feat: remove unnecessary config copy (thanks to deployer-extended-typo3 upgrade)

--- recipe/deploy_prepare_typo3.php
+++ recipe/deploy_prepare_typo3.php
@@ -122,14 +122,6 @@ task('deploy:prepare:typo3', function () {
             run('echo "TYPO3_CONF_VARS__DB__Connections__Default__user=\'' . $databaseUser . '\'" >> {{deploy_path}}/shared/.env');
             run('echo "TYPO3_CONF_VARS__DB__Connections__Default__password=\'' . $databasePassword . '\'" >> {{deploy_path}}/shared/.env');
             run('echo "TYPO3_CONF_VARS__DB__Connections__Default__dbname=\'' . $databaseName . '\'" >> {{deploy_path}}/shared/.env');
-
-            run('echo "" >> {{deploy_path}}/shared/.env');
-            run('echo "# Copy configuration for deployer" >> {{deploy_path}}/shared/.env');
-            run('echo "TYPO3__DB__Connections__Default__dbname=\$TYPO3_CONF_VARS__DB__Connections__Default__dbname" >> {{deploy_path}}/shared/.env');
-            run('echo "TYPO3__DB__Connections__Default__host=\$TYPO3_CONF_VARS__DB__Connections__Default__host" >> {{deploy_path}}/shared/.env');
-            run('echo "TYPO3__DB__Connections__Default__password=\$TYPO3_CONF_VARS__DB__Connections__Default__password" >> {{deploy_path}}/shared/.env');
-            run('echo "TYPO3__DB__Connections__Default__port=\$TYPO3_CONF_VARS__DB__Connections__Default__port" >> {{deploy_path}}/shared/.env');
-            run('echo "TYPO3__DB__Connections__Default__user=\$TYPO3_CONF_VARS__DB__Connections__Default__user" >> {{deploy_path}}/shared/.env');
         }
     }
 
