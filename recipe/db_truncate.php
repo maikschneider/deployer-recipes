559c7d8f6d2290af5395541b10bf2b38937943a1
Maik Schneider <m.schneider@blueways.de>
2021-12-07 11:47:03 +0100

    feat: override db:truncate task, add check for active database connection

---
+++ recipe/db_truncate.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Deployer;
+
+use SourceBroker\DeployerExtendedDatabase\Utility\ArrayUtility;
+use SourceBroker\DeployerExtendedDatabase\Utility\ConsoleUtility;
+use SourceBroker\DeployerExtendedDatabase\Utility\DatabaseUtility;
+use TYPO3\CMS\Core\Exception;
+
+/*
+ * @see https://github.com/sourcebroker/deployer-extended-database#db-truncate
+ */
+task('db:truncate', function () {
+    $arrayUtility = new ArrayUtility();
+    $databaseUtility = new DatabaseUtility();
+    if (empty(get('argument_stage'))) {
+
+        // abort if database connection fails (in case of activated driver_cms and empty database)
+        try {
+            $databaseConfigs = get('db_databases_merged');
+        } catch (\Exception $e) {
+            return;
+        }
+
+        foreach ($databaseConfigs as $databaseConfig) {
+            if (isset($databaseConfig['truncate_tables']) && is_array($databaseConfig['truncate_tables'])) {
+                $truncateTables = $arrayUtility->filterWithRegexp(
+                    $databaseConfig['truncate_tables'],
+                    $databaseUtility->getTables($databaseConfig)
+                );
+                if (!empty($truncateTables)) {
+                    foreach ($truncateTables as $truncateTable) {
+                        runLocally(sprintf(
+                            'export MYSQL_PWD=%s && %s -h%s -P%s -u%s -D%s -e %s',
+                            escapeshellarg($databaseConfig['password']),
+                            get('local/bin/mysql'),
+                            escapeshellarg($databaseConfig['host']),
+                            escapeshellarg((isset($databaseConfig['port']) && $databaseConfig['port']) ? $databaseConfig['port'] : 3306),
+                            escapeshellarg($databaseConfig['user']),
+                            escapeshellarg($databaseConfig['dbname']),
+                            escapeshellarg('TRUNCATE ' . $truncateTable)
+                        ));
+                    }
+                    writeln('<info>Truncated tables: ' . implode(',', $truncateTables) . '</info>');
+                }
+            }
+        }
+    } else {
+        $verbosity = (new ConsoleUtility())->getVerbosityAsParameter();
+        $activePath = get('deploy_path') . '/' . (test('[ -L {{deploy_path}}/release ]') ? 'release' : 'current');
+        run('cd ' . $activePath . ' && {{bin/php}} {{bin/deployer}} db:truncate ' . $verbosity);
+    }
+})->desc('Truncate tables defined in "truncate_tables" variable');
